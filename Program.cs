0f0fdb9fe6e4335ee310346d6de3b58d44e9c2e0
Rodger Gu <rgu663@aucklanduni.ac.nz>
2020-02-15 13:11:09 +1300

    Initialised goon generator projectr

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ONVO_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
