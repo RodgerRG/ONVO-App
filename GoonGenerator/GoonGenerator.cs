f3e41cedefdc9b1263779392b02c8a9f5ddb3a27
Rodger Gu <rgu663@aucklanduni.ac.nz>
2020-02-15 14:32:01 +1300

    Added in code for generating random numbers for the goon

--- GoonGenerator/GoonGenerator.cs
+++ GoonGenerator/GoonGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 namespace ONVO_App.GoonGenerator
 {
     public class GoonGenerator
@@ -6,8 +7,79 @@ namespace ONVO_App.GoonGenerator
 
         }
 
-        public Goon generateGoon() {
-            
+        public Goon generateGoon(int level, int minStat, int maxStat, int minHP, int maxHP, int minStartRP, int maxStartRP, int minMaxRP, int maxMaxRP, int minRPMod, int maxRPMod) {
+            Random rng = new Random();
+            int statsToGo = 25 + level;
+            int[] stats = new int[5];
+
+            if(maxStat * 5 < statsToGo) {
+                throw new ArgumentException("cannot achieve required stats with the given max!");
+            }
+
+            if(minStat > maxStat) {
+                throw new ArgumentException("maxStat cannot be less than minStat");
+            }
+
+            for(int i = 0; i < 5; i++) {
+                int stat = rng.Next(minStat, maxStat);
+
+                while(statsToGo - stat < 0) {
+                    stat = rng.Next(minStat, maxStat);
+                }
+
+                stats[i] = stat;
+            }
+
+            while(!checkSum(stats, 25 + level)) {
+                int ind = rng.Next(0,5);
+                if(stats[ind] + 1 > maxStat) {
+                    continue;
+                } else {
+                    stats[ind] += 1;
+                }
+            }
+
+            int hp = rng.Next(minHP, maxHP + 1);
+            int startRP = rng.Next(minStartRP, maxStartRP + 1);
+            int maxRP = rng.Next(minMaxRP, maxMaxRP + 1);
+            int rpMod = rng.Next(minRPMod, maxRPMod + 1);
+
+            if(maxRP < startRP) {
+                int temp = startRP;
+                startRP = maxRP;
+                maxRP = temp;
+            }
+
+
+            return makeGoon(stats, hp, startRP, maxRP, rpMod);
+        }
+
+        private bool checkSum(int[] stats, int total) {
+            int sum = 0;
+            foreach(int stat in stats) {
+                sum += stat;
+            }
+
+            if(sum == total) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private Goon makeGoon(int[] stats, int hp, int startRP, int maxRP, int rpMod) {
+            Goon.setCharm(stats[0]);
+            Goon.setInt(stats[1]);
+            Goon.setPow(stats[2]);
+            Goon.setSpeed(stats[3]);
+            Goon.setTech(stats[4]);
+
+            Goon.setHP(hp);
+            Goon.setCurrentRP(startRP);
+            Goon.setMaxRP(maxRP);
+            Goon.setRPMod(rpMod);
+
+            return Goon.makeGoon();
         }
     }
 }
--- GoonGenerator/QuirkGenerator.cs
+++ GoonGenerator/QuirkGenerator.cs
@@ -1,9 +1,32 @@
+using System;
 namespace ONVO_App.GoonGenerator
 {
     public static class QuirkGenerator
     {
         public static string generateQuirk() {
-            return null;
+            Random rng = new Random();
+            int type = rng.Next(1, 4);
+            string quirk = "";
+            
+            switch((QuirkType) type) {
+                case QuirkType.EMITTER:
+                break;
+                case QuirkType.TRANSFORMATION:
+                break;
+                case QuirkType.MUTATION:
+                break;
+            }
+
+
+
+            return quirk;
+        }
+
+        public enum QuirkType {
+            EMITTER = 1, 
+            TRANSFORMATION = 2, 
+            MUTATION = 3,
+
         }
     }
 }
