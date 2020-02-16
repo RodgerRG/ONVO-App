50bc6d5dafb96d494aee23459767c6b8cc2c541f
Rodger Gu <rgu663@aucklanduni.ac.nz>
2020-02-16 17:25:09 +1300

    Added text files for containing lists of words to generate quirks from, added file parsing to the quirkgenerator to create a list of these nouns

--- GoonGenerator/QuirkGenerator.cs
+++ GoonGenerator/QuirkGenerator.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections;
+using System.IO;
 namespace ONVO_App.GoonGenerator
 {
     public static class QuirkGenerator
@@ -10,16 +11,83 @@ namespace ONVO_App.GoonGenerator
             
             switch((QuirkType) type) {
                 case QuirkType.EMITTER:
-                break;
+                    quirk = generateEmitterQuirk();
+                    break;
                 case QuirkType.TRANSFORMATION:
-                break;
+                    quirk = generateTransformationQuirk();
+                    break;
                 case QuirkType.MUTATION:
-                break;
+                    quirk = generateMutationQuirk();
+                    break;
             }
 
-
-
             return quirk;
+        }
+
+        private static string generateEmitterQuirk() {
+            string output = "";
+            return output;
+        }
+
+        private static string generateMutationQuirk() {
+            string output = "";
+            return output;
+        }
+
+        private static string generateTransformationQuirk() {
+            string output = "Transform into a ";
+            
+            string currentDir = System.IO.Directory.GetCurrentDirectory();
+            string[] dirs = System.IO.Directory.GetDirectories(currentDir);
+            string[] files = null;
+            foreach(string s in dirs) {
+                if(s.Contains("res")) {
+                    files = System.IO.Directory.GetFiles(s);
+                }
+            }
+
+            string nounsFilePath = "";
+            string quirksFilePath = "";
+
+            foreach(string s in files) {
+                if(s.Contains("nouns")) {
+                    nounsFilePath = s;
+                }
+                if(s.Contains("quirks")) {
+                    quirksFilePath = s;
+                }
+            }
+
+            ArrayList nouns = getNouns(nounsFilePath);
+            ArrayList quirks = getQuirks(quirksFilePath);
+            
+
+            return output;
+        }
+
+        /**
+        TODO: Add threading to these two methods as they manipulate data streams
+         */
+        private static ArrayList getNouns(string path) {
+            StreamReader nounStream = new StreamReader(File.OpenRead(path));
+            ArrayList nouns = new ArrayList();
+
+            while(!nounStream.EndOfStream) {
+                nouns.Add(nounStream.ReadLine());
+            }
+
+            return nouns;
+        }
+
+        private static ArrayList getQuirks(string path) {
+            StreamReader quirkStream = new StreamReader(File.OpenRead(path));
+            ArrayList quirks = new ArrayList();
+
+            while(!quirkStream.EndOfStream) {
+                quirks.Add(quirkStream.ReadLine());
+            }
+
+            return quirks;
         }
 
         public enum QuirkType {
